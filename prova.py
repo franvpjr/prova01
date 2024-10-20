ed59d5b8d50aca50338d073ac356258fcfaf46d0
franvpjr <franvpjr@hotmail.com>
2024-10-20 11:04:10 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,7 @@
+base = int(input("digite o tamanho da base do retangulo: "))
+altura = int(input("digite o tamanho da altura do retangulo: "))
+
+area = base * altura
+
+
+print(f"A área do retângulo é {area} unidades quadradas.")
